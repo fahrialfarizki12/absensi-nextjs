3a501ea8f2b9fb30110bc550a0a5faa1b69ab6c9
fahrialfarizki <fahrialfarizki858@gmail.com>
2025-12-15 22:40:14 +0700

    Update layout.jsx

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,7 +1,7 @@
 // app/dashboard/layout.tsx
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
-import ClientLayout from "./ClientLayout";
+import ClientLayout from "./ClientLayout.jsx";
 
 export default function Layout({ children }) {
   const token = cookies().get("token")?.value;
