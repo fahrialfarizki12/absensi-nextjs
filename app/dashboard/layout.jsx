7643f836863ef87c211d3250d85985fa5f357852
fahrialfarizki <fahrialfarizki858@gmail.com>
2025-12-15 22:29:14 +0700

    Update layout.jsx

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -2,12 +2,17 @@
 import Navbar from "@/components/dashboard/Navbar";
 import ToasyProvider from "@/components/ToasyProvider"; 
 import NextTopLoader from "nextjs-toploader";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 export const metadata = {
   title: "Dashboard | HOME",
   description: "Dashboard Prakerind SMKS PGRI I TRANSPRAM II",
 };
 
 export default function RootLayout({ children }) {
+  const token = cookies().get("token")?.value;
+
+  if (!token) redirect("/auth/login")
   return (
     <>
         <Navbar />
