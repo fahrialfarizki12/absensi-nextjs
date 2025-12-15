2a30d27095c3e2a8d9d125856a111697e69609c9
fahrialfarizki <fahrialfarizki858@gmail.com>
2025-12-15 22:28:43 +0700

    Update layout.jsx

--- app/auth/layout.jsx
+++ app/auth/layout.jsx
@@ -1,5 +1,6 @@
 import ToasyProvider from "@/components/ToasyProvider";
-
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 export const metadata = {
   title: "Auth Login & Register",
   description: "Pendaftaran Akun Prakerind Siswa SMKS PGRI I TRANSPRAM II",
@@ -9,6 +10,9 @@ export const metadata = {
 };
 
 export default function AuthLayout({ children }) {
+  const token = cookies().get("token")?.value;
+
+  if (token) redirect("/dashboard")
   return (
     <main>
       <ToasyProvider />
