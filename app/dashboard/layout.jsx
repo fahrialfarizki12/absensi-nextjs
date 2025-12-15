f5c6dd45f888732bc64024a77d3214629c61f0f2
fahrialfarizki <fahrialfarizki858@gmail.com>
2025-12-15 22:51:33 +0700

    Update layout.jsx

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,11 +1,25 @@
-// app/dashboard/layout.tsx
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
-import ClientLayout from "./ClientLayout.jsx";
+import Navbar from "@/components/dashboard/Navbar";
+import ToasyProvider from "@/components/ToasyProvider";
+import NextTopLoader from "nextjs-toploader";
+export const metadata = {
+  title: "Dashboard | HOME",
+  description: "Dashboard Prakerind SMKS PGRI I TRANSPRAM II",
+};
 
-export default function Layout({ children }) {
+export default function RootLayout({ children }) {
   const token = cookies().get("token")?.value;
-  if (!token) redirect("/auth/login");
 
-  return <ClientLayout>{children}</ClientLayout>;
+  if (!token) redirect("/auth/login");
+  return (
+    <>
+      <Navbar />
+      <main className="md:ml-80 md:w-[calc(100%-23rem)] mt-20">
+        <ToasyProvider />
+        <NextTopLoader color="#2F855A" />
+        {children}
+      </main>
+    </>
+  );
 }
