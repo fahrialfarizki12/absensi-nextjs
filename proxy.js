9425c2bb22d1f854e038895d758c41d46cb3bcc7
fahrialfarizki <fahrialfarizki858@gmail.com>
2025-12-16 16:31:55 +0700

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,14 +1,18 @@
 import { NextResponse } from "next/server";
 
-export function proxy(request) {
+export function middleware(request) {
   const { pathname } = request.nextUrl;
 
   const token = request.cookies.get("token")
   const roles = request.cookies.get("roles")
-console.log(token)
+
+  const isAuthRoute = pathname.startsWith("/auth");
 
   if (!token && !isAuthRoute) {
-    return NextResponse.redirect(new URL("/auth/login", request.url));
+    return NextResponse.redirect(
+      new URL("/auth/login", request.url)
+    );
   }
-}
 
+  return NextResponse.next();
+}
