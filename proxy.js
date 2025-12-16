358761226051acda7fd7389d52368d45acfb837b
fahrialfarizki <fahrialfarizki858@gmail.com>
2025-12-16 16:33:11 +0700

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-export function middleware(request) {
+export function proxy(request) {
   const { pathname } = request.nextUrl;
 
   const token = request.cookies.get("token")
@@ -16,3 +16,4 @@ export function middleware(request) {
 
   return NextResponse.next();
 }
+
