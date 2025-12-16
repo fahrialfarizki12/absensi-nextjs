7511c443e0cf2db6e663682aa494def08746b3de
fahrialfarizki <fahrialfarizki858@gmail.com>
2025-12-16 16:28:58 +0700

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -4,7 +4,11 @@ export function proxy(request) {
   const { pathname } = request.nextUrl;
 
   const token = request.cookies.get("token")
-  const roles = request.cookies.get("roles")?.value;
+  const roles = request.cookies.get("roles")
 console.log(token)
 
+  if (!token && !isAuthRoute) {
+    return NextResponse.redirect(new URL("/auth/login", request.url));
+  }
 }
+
