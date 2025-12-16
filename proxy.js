f9964b4a75a3523d87adc79ec6ca57ddf2e36af8
fahrialfarizki <fahrialfarizki858@gmail.com>
2025-12-16 16:37:26 +0700

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -3,17 +3,26 @@ import { NextResponse } from "next/server";
 export function proxy(request) {
   const { pathname } = request.nextUrl;
 
-  const token = request.cookies.get("token")
-  const roles = request.cookies.get("roles")
+  const token = request.cookies.get("token")?.value;
+  const roles = request.cookies.get("roles")?.value;
 
   const isAuthRoute = pathname.startsWith("/auth");
+  const isDashboardRoute = pathname.startsWith("/dashboard");
 
-  if (!token && !isAuthRoute) {
-    return NextResponse.redirect(
-      new URL("/auth/login", request.url)
-    );
+  
+  /* ===============================
+     2️⃣ BELUM LOGIN → LARANG DASHBOARD
+  =============================== */
+  if (!token && isDashboardRoute) {
+    return NextResponse.redirect(new URL("/auth/login", request.url));
   }
 
   return NextResponse.next();
 }
 
+/* ===============================
+   4️⃣ ROUTE MATCHER
+=============================== */
+export const config = {
+  matcher: ["/dashboard/:path*", "/auth/:path*"],
+};
